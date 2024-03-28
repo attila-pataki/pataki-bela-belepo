cacd4a3e5dce67164c1903efb0e7f16af044777c
beci-a-pataki <attila.pataki@hotmail.co.uk>
2024-03-28 09:33:15 +0100

    welcome page added

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,9 +2,11 @@ import { Routes } from '@angular/router';
 
 import { AddPageComponent } from './add-page/add-page.component';
 import { NewQueryComponent } from './new-query/new-query.component';
+import { WelcomeScreenComponent } from './welcome-screen/welcome-screen.component';
 
 export const routes: Routes = [
     {path: 'add-new', component: AddPageComponent},
     {path: 'new-query', component: NewQueryComponent},
+    {path: '', component: WelcomeScreenComponent, pathMatch: 'full'},
     
 ];
---
+++ src/app/welcome-screen/welcome-screen.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-welcome-screen',
+  standalone: true,
+  imports: [],
+  templateUrl: './welcome-screen.component.html',
+  styleUrl: './welcome-screen.component.css'
+})
+export class WelcomeScreenComponent {
+
+}
